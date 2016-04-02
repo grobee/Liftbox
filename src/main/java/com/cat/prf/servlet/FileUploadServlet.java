80a43662895a4d4e239d145d575e61786ae3c96e
masm64 <grobi1993@gmail.com>
2016-04-02 18:42:34 +0200

    added UTF-8 support for uploads
    * added UTF-8 support for file names
    * added UTF-8 support for file body

--- src/main/java/com/cat/prf/servlet/FileUploadServlet.java
+++ src/main/java/com/cat/prf/servlet/FileUploadServlet.java
@@ -1,5 +1,7 @@
 package com.cat.prf.servlet;
 
+import org.apache.commons.lang3.StringEscapeUtils;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.MultipartConfig;
 import javax.servlet.annotation.WebServlet;
@@ -7,28 +9,26 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.Part;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.PrintWriter;
-import java.util.Collection;
+import java.io.*;
 import java.util.logging.Logger;
 
-@SuppressWarnings("serial")
 @WebServlet(name = "fileUploadServlet", urlPatterns = {"/upload"})
 @MultipartConfig
 public class FileUploadServlet extends HttpServlet {
+    private static final Logger LOGGER = Logger.getLogger(FileUploadServlet.class.getSimpleName());
+    private static final long serialVersionUID = 23186242707046333L;
+
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        req.setCharacterEncoding("UTF-8");
         int n = 0;
         for (Part file : req.getParts()) {
-            try (InputStream is = file.getInputStream();
-                 FileOutputStream fos = new FileOutputStream("E:\\upload\\" + file.getSubmittedFileName())) {
+            String fileName = new String(file.getSubmittedFileName().getBytes("iso-8859-1"), "UTF-8");
+            try (DataInputStream dis = new DataInputStream(file.getInputStream());
+                 DataOutputStream dos = new DataOutputStream(new FileOutputStream("E:\\upload\\" + fileName))) {
                 byte[] buffer = new byte[1024];
                 int r;
-                while ((r = is.read(buffer)) != -1) {
-                    fos.write(buffer, 0, r);
+                while ((r = dis.read(buffer)) != -1) {
+                    dos.write(buffer, 0, r);
                 }
                 n++;
             }
