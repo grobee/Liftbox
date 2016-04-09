071ae7b2fd3890b684b522f94bde5cbe6a525d7f
masm64 <grobi1993@gmail.com>
2016-04-09 14:12:00 +0200

    finished basic design
    * added a navigation bar
    * added links for the various pages
    * added a new template for the login page, which has no navigation bar
    * added a menu / navigation bar bean, which contains the page attributes

---
+++ src/main/java/com/cat/prf/controller/MenuBean.java
@@ -0,0 +1,41 @@
+package com.cat.prf.controller;
+
+import javax.annotation.PostConstruct;
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Named;
+import java.util.ArrayList;
+import java.util.List;
+
+@Named
+@ApplicationScoped
+public class MenuBean {
+    private List<Page> pages = new ArrayList<>();
+
+    @PostConstruct
+    public void initialize() {
+        pages.add(new Page("Upload", "/upload.xhtml"));
+        pages.add(new Page("Files", "/listfiles.xhtml"));
+    }
+
+    public List<Page> getPages() {
+        return pages;
+    }
+
+    public class Page {
+        private String title;
+        private String viewId;
+
+        Page(String title, String viewId) {
+            this.title = title;
+            this.viewId = viewId;
+        }
+
+        public String getTitle() {
+            return title;
+        }
+
+        public String getViewId() {
+            return viewId;
+        }
+    }
+}
