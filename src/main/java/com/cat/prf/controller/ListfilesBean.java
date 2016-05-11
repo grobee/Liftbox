d4756467f2088e11d4bfa72d0b06af285ee2c770
Grobee <grobi1993@gmail.com>
2016-05-11 15:44:50 +0200

    some leftover from the previous commit

--- src/main/java/com/cat/prf/controller/ListfilesBean.java
+++ src/main/java/com/cat/prf/controller/ListfilesBean.java
@@ -21,8 +21,6 @@ import java.io.Serializable;
 import java.sql.Date;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.SortedSet;
-import java.util.TreeSet;
 import java.util.logging.Logger;
 
 
